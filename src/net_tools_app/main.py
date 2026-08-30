4791f124d266784d2c9d97a3b04bd32a889177cb
Jemuel B Jeroham <jemuelbjeroham@outlook.com>
2026-08-31 03:57:05 +0530

    feat: initialize RAG service on application startup (LIFESPAN)

--- src/net_tools_app/main.py
+++ src/net_tools_app/main.py
@@ -1,4 +1,13 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
+from netops_ingestion.services.factory import create_knowledge_base_service
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    app.state.knowledge_base = create_knowledge_base_service(embedding_model="Qwen/Qwen3-Embedding-0.6B", collection_name="netops_kb", persist_directory="../net-ops-kb-rag/storage/chroma")
+    yield
 
 app = FastAPI(title="Network Operations Tools API app", version="0.1.0")
 
